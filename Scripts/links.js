19ec9d85b530c1f3dad20dd7417fca12587dddd7
Igor <igorleonardo031@gmail.com>
2022-10-12 20:03:15 -0300

    first commit

---
+++ Scripts/links.js
@@ -0,0 +1,21 @@
+let links = [li1,li2,li3];
+
+li1 = document.getElementById ('li1');
+
+li1.addEventListener('click', (e)=>{
+
+    location = "../projeto/form.html"
+})
+
+li2 = document.getElementById('li2');
+
+li2.addEventListener('click', (e)=>{
+window.location ='../projeto/form.html'
+})
+
+
+li3 = document.getElementById('li3');
+
+li3.addEventListener('click', (e) =>{
+    location ="../projeto/form.html"
+})
